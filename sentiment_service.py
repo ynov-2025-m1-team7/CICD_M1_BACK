f8f73b76cc0dbcdea34b8c0969aa223aaf33d289
cheikhhhhh <cheikhchouiref@gmail.com>
2025-05-05 14:51:09 +0200

    CORs et api analyse

--- sentiment_service.py
+++ sentiment_service.py
@@ -1,17 +1,15 @@
 from flask import Flask, request, jsonify
 from textblob import TextBlob
-from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app)
 
-@app.route("/analyze", methods=["POST"])
+@app.route('/analyze', methods=['POST'])
 def analyze():
     data = request.get_json()
-    commentaire = data.get("text", "")
-    blob = TextBlob(commentaire)
-    polarite = blob.sentiment.polarity
-    return jsonify({"score": polarite})
+    text = data.get("text", "")
+    blob = TextBlob(text)
+    score = blob.sentiment.polarity
+    return jsonify({"score": score})
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
